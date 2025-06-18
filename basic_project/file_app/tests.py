2a24746c1bae2b0002fc88c8f2e416fbfdecbc2c
Bogdan <Bogdangulenko311@gmail.com>
2025-06-18 18:05:44 +0000

    Innitial commit

--- basic_project/file_app/tests.py
+++ basic_project/file_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import File
 
-# Create your tests here.
+class FileModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+    def test_create_file(self):
+        test_file = SimpleUploadedFile("testfile.txt", b"file_content")
+        file_obj = File.objects.create(
+            user=self.user,
+            file=test_file,
+            category='document'
+        )
+        self.assertEqual(file_obj.user.username, 'testuser')
+        self.assertEqual(file_obj.category, 'document')
+        self.assertTrue(file_obj.file.name.endswith("testfile.txt"))
--- basic_project/file_app/views.py
+++ basic_project/file_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import File
+from .forms import FileUploadForm
 
-# Create your views here.
+
+@login_required
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_obj = form.save(commit=False)
+            file_obj.user = request.user
+            file_obj.save()
+            return redirect('file_list')
+    else:
+        form = FileUploadForm()
+    return render(request, 'file_app/upload.html', {'form': form})
+
+@login_required
+def file_list(request):
+    category = request.GET.get('category')
+    files = File.objects.filter(user=request.user)
+    if category:
+        files = files.filter(category=category)
+    return render(request, 'file_app/file_list.html', {'files': files})
