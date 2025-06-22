00051b5db1deb50cb78cb322acd9819d0b53c467
Bogdan <Bogdangulenko311@gmail.com>
2025-06-22 08:39:07 +0000

    Переміщено шаблони у правильну папку, оновлено views.py

--- basic_project/file_app/views.py
+++ basic_project/file_app/views.py
@@ -1,7 +1,27 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import File
+from .forms import FileUploadForm
 
-urlpatterns = [
-    path('upload/', views.upload_file, name='upload_file'),
-    path('files/', views.file_list, name='file_list'),
-]
+
+@login_required
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_obj = form.save(commit=False)
+            file_obj.user = request.user
+            file_obj.save()
+            return redirect('file_list')
+    else:
+        form = FileUploadForm()
+    return render(request, 'file_app/upload.html', {'form': form})
+
+@login_required
+def file_list(request):
+    category = request.GET.get('category')
+    files = File.objects.filter(user=request.user)
+    if category:
+        files = files.filter(category=category)
+    return render(request, 'file_app/file_list.html', {'files': files})
+
