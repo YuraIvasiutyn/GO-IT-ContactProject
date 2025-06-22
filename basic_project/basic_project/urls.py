b2eda18246619fe56310ce4048018f98fa89f2b1
Bogdan <Bogdangulenko311@gmail.com>
2025-06-22 08:50:32 +0000

    Тестові зміни файла urls.py в папці basic_project

--- basic_project/basic_project/urls.py
+++ basic_project/basic_project/urls.py
@@ -29,3 +29,6 @@ urlpatterns = [
     path('news/', include('scraping_app.urls')),
     path('file/', include('file_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
