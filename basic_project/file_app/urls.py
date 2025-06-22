7ecece472b8e39daed2460d8214316d91059844b
Bogdan <Bogdangulenko311@gmail.com>
2025-06-22 08:42:51 +0000

    Змінено urls.py

--- basic_project/file_app/urls.py
+++ basic_project/file_app/urls.py
@@ -1,14 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include
+#from django.contrib import admin
+#from django.urls import path, include
+
+#from django.conf import settings
+#from django.conf.urls.static import static
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('contact_app/', include('contact_app.urls')),
+#    path('', include('file_app.urls')),  
+#]
+#
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('contact_app/', include('contact_app.urls')),
-    path('', include('file_app.urls')),  
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
