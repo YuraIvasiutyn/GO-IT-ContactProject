d2fe7d2acf98ce5a22d466bf44e1af897515c779
YuraIvasiutyn <yuraivasyutin221@gmail.com>
2025-06-22 11:40:44 +0200

    file_app зміни

--- basic_project/file_app/urls.py
+++ basic_project/file_app/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('upload/', views.upload_file, name='upload_file'),
