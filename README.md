927f2d2b1200190c246e1495cb2308256ddac6b5
ExtymAndriy <petrovskiyandriyko@gmail.com>
2025-06-25 15:20:34 +0300

    first commit

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ python manage.py createsuperuser
 ```
 4. Виконуємо перший запуск застосунку на перевірку коректності
 ```cmd
-python3 manage.py runserver
+python manage.py runserver
 ```
 * Шлях до застосутнку - http://127.0.0.1:8000/
 * Шлях до адмін-панелі - http://127.0.0.1:8000/admin/login/?next=/admin/
--- basic_project/basic_project/urls.py
+++ basic_project/basic_project/urls.py
@@ -21,7 +21,6 @@ from . import views
 from django.conf import settings
 # from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main, name='main'),
@@ -32,6 +31,7 @@ urlpatterns = [
     path('contact_app/', include('contact_app.urls')),
     path('news/', include('scraping_app.urls')),
     path('file/', include('file_app.urls')),
+    path('users/', include('users.urls'))
 ]
 
 # if settings.DEBUG:
