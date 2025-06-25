b9c9670aad87082c20c11500748e16b112982f7c
ExtymAndriy <petrovskiyandriyko@gmail.com>
2025-06-25 16:35:22 +0300

    second commit

--- basic_project/users/views.py
+++ basic_project/users/views.py
@@ -7,14 +7,15 @@ from django.contrib import messages
 
 def signupuser(request):
     if request.user.is_authenticated:
-        return redirect(to='basic_project:main')
+        return redirect(to='main')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(to='basic_project:main')
+            return redirect(to='main')
         else:
+            print(":x: Signup Form Errors:", form.errors)  # :point_left: Додай для дебагу
             return render(request, 'users/signup.html', context={"form": form})
 
     return render(request, 'users/signup.html', context={"form": RegisterForm()})
@@ -23,23 +24,24 @@ def signupuser(request):
 def loginuser(request):
     if request.user.is_authenticated:
         return redirect(to='basic_project:main')
-
     if request.method == 'POST':
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            messages.error(request, 'Username or password didn\'t match')
-            return redirect(to='users:login')
-
-        login(request, user)
-        return redirect(to='basic_project:main')
-
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user is None:
+                messages.error(request, 'Username or password didn\'t match')
+                return redirect(to='users:login')
+            login(request, user)
+            return redirect(to='main')
+        else:
+            print(form.errors)
     return render(request, 'users/login.html', context={"form": LoginForm()})
 
 
 @login_required
 def logoutuser(request):
     logout(request)
-    return redirect(to='basic_project:main')
+    return redirect(to='main')
 
 
 
