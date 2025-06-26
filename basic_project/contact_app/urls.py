d8703eb7ba8aef639915b5783b4cfc6101412728
ihor <i0962112822@gmail.com>
2025-06-26 18:54:03 +0300

    add typing

--- basic_project/contact_app/urls.py
+++ basic_project/contact_app/urls.py
@@ -1,11 +1,11 @@
+from typing import List
 from django.urls import path
-
+from django.urls.resolvers import URLPattern
 from . import views
 
+app_name: str = 'contact_app'
 
-app_name = 'contact_app'
-
-urlpatterns = [
+urlpatterns: List[URLPattern] = [
     path('', views.main, name='main'),
     path('add_contact/', views.add_contact, name='add_contact'),
     path('edit_contact/<int:pk>', views.edit_contact, name='edit_contact'),
