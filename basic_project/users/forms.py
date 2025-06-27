bf868860e0d3c1c4915364ccebd32d92bfcc362f
Viktor Chayun <vchayun@gmail.com>
2025-06-28 00:07:28 +0300

    added rset password function

--- basic_project/users/forms.py
+++ basic_project/users/forms.py
@@ -19,9 +19,38 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("A user with that email already exists.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm = cleaned_data.get("password_confirm")
+        if password and confirm and password != confirm:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
+
 
 class LoginForm(AuthenticationForm):
 
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class PasswordResetRequestForm(forms.Form):
+    email = forms.EmailField(label="Enter your email", max_length=254)
+
+
+class SetNewPasswordForm(forms.Form):
+    new_password = forms.CharField(widget=forms.PasswordInput, label="New Password")
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['new_password'] != cleaned_data['confirm_password']:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
--- basic_project/users/urls.py
+++ basic_project/users/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('logout/', views.logoutuser, name='logout'),
     path('signup-success/', views.signup_success, name='signup-success'),
     path('activate/<uidb64>/<token>/', views.activate_user, name='activate_user'),
+
+    path('password-reset/', views.password_reset_request, name='password_reset'),
+    path('password-reset/done/', views.password_reset_done, name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
 ]
